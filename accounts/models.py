46d2628cd00bc2619db2dc980ee32f2082e29a09
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-08 16:58:57 +0300

    feat: implement custom UserManager and add RegisterSerializer for user registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,36 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required.')
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            full_name=full_name,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, full_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        return self.create_user(
+            email=email,
+            full_name=full_name,
+            password=password,
+            **extra_fields
+        )
 
 
 class User(AbstractUser):
@@ -30,6 +61,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name']
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
