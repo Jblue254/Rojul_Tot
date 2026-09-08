667972eb56c4f8043547b309e9ed9cc1fbf64c08
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-09 00:00:13 +0300

    feat: register Rental model in admin with display, filter, and search options

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Rental
 
-# Register your models here.
+
+@admin.register(Rental)
+class RentalAdmin(admin.ModelAdmin):
+    list_display = (
+        'customer',
+        'machine',
+        'start_date',
+        'end_date',
+        'quantity',
+        'total_price',
+        'status',
+    )
+
+    list_filter = ('status', 'start_date', 'end_date')
+    search_fields = (
+        'customer__email',
+        'machine__name',
+    )
