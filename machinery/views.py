39ade7ce4f83ab21ede76795487a6ed6dadea230
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-08 23:20:21 +0300

    feat: implement views for Category and Machine models with permissions

--- machinery/views.py
+++ machinery/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Category, Machine
+from .serializers import CategorySerializer, MachineSerializer
 
-# Create your views here.
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class MachineListCreateView(generics.ListCreateAPIView):
+    queryset = Machine.objects.all()
+    serializer_class = MachineSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class MachineDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Machine.objects.all()
+    serializer_class = MachineSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
