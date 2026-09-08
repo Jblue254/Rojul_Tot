3cd01b216891a2f51a14d5903b358f6c86a6be33
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-09 00:13:13 +0300

    feat: add validation and total price calculation to RentalSerializer

--- rentals/serializers.py
+++ rentals/serializers.py
@@ -38,3 +38,39 @@ class RentalSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
+
+    def validate(self, data):
+        if data['end_date'] <= data['start_date']:
+            raise serializers.ValidationError({
+                'end_date': 'End date must be after start date.'
+            })
+
+        if data['quantity'] <= 0:
+            raise serializers.ValidationError({
+                'quantity': 'Quantity must be greater than zero.'
+            })
+
+        if data['quantity'] > data['machine'].quantity:
+            raise serializers.ValidationError({
+                'quantity': 'Requested quantity is not available.'
+            })
+
+        return data
+
+    def create(self, validated_data):
+        machine = validated_data['machine']
+        start_date = validated_data['start_date']
+        end_date = validated_data['end_date']
+        quantity = validated_data['quantity']
+
+        rental_days = (end_date - start_date).days
+
+        total_price = (
+            machine.price_per_day
+            * quantity
+            * rental_days
+        )
+
+        validated_data['total_price'] = total_price
+
+        return Rental.objects.create(**validated_data)
