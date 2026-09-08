5a632bfc6d1cc44b660525b20167358e45fa6024
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-09 00:03:47 +0300

    feat: implement RentalListCreateView and RentalDetailView for managing rentals

--- rentals/views.py
+++ rentals/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Rental
+from .serializers import RentalSerializer
+
+
+class RentalListCreateView(generics.ListCreateAPIView):
+    serializer_class = RentalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Rental.objects.filter(customer=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user)
+
+
+class RentalDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RentalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Rental.objects.filter(customer=self.request.user)
