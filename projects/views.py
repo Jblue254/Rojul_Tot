70dc12b89519281036b47dd597bd9c4e077e17d4
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-10 23:53:28 +0300

    feat: implement ProjectListCreateView and ProjectDetailView for project management

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role in ['MANAGER', 'ADMIN']:
+            return Project.objects.all()
+
+        return Project.objects.filter(customer=user)
+
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user)
+
+
+class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role in ['MANAGER', 'ADMIN']:
+            return Project.objects.all()
+
+        return Project.objects.filter(customer=user)
