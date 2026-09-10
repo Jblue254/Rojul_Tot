265a9d3650d902441c1d958b8d3163070806573f
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-11 00:17:27 +0300

    feat: add MaintenanceSerializer for managing maintenance records and validation

--- machinery/serializers.py
+++ machinery/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Category, Machine
+from .models import Category, Machine, Maintenance
+
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -41,3 +42,46 @@ class MachineSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
+
+class MaintenanceSerializer(serializers.ModelSerializer):
+    machine_name = serializers.CharField(
+        source='machine.name',
+        read_only=True
+    )
+
+    class Meta:
+        model = Maintenance
+        fields = [
+            'id',
+            'machine',
+            'machine_name',
+            'service_type',
+            'description',
+            'cost',
+            'service_date',
+            'next_service_date',
+            'status',
+            'created_at',
+        ]
+
+        read_only_fields = [
+            'id',
+            'machine_name',
+            'created_at',
+        ]
+
+    def validate(self, data):
+        next_service_date = data.get('next_service_date')
+        service_date = data.get('service_date')
+
+        if (
+            next_service_date and
+            service_date and
+            next_service_date <= service_date
+        ):
+            raise serializers.ValidationError({
+                'next_service_date':
+                'Next service date must be after service date.'
+            })
+
+        return data
