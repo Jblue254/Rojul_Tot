fef2e2956a4f261e0e941465fe50799b00a4dfc7
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-10 22:37:14 +0300

    feat: implement Cart and CartItem views with creation logic for handling cart operations

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-
 from .models import Order
 from .serializers import OrderSerializer
-
+from .models import Cart, CartItem
+from .cart_serializers import CartSerializer, CartItemSerializer
+from drawings.models import Drawing
 
 class OrderListCreateView(generics.ListCreateAPIView):
     serializer_class = OrderSerializer
@@ -32,3 +33,47 @@ class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
             return Order.objects.all()
 
         return Order.objects.filter(customer=user)
+    
+class CartView(generics.RetrieveAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        cart, created = Cart.objects.get_or_create(
+            customer=self.request.user
+        )
+        return cart
+
+
+class CartItemCreateView(generics.CreateAPIView):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        cart, created = Cart.objects.get_or_create(
+            customer=self.request.user
+        )
+
+        drawing = serializer.validated_data['drawing']
+
+        cart_item, created = CartItem.objects.get_or_create(
+            cart=cart,
+            drawing=drawing
+        )
+
+        if created:
+            cart_item.quantity = serializer.validated_data['quantity']
+        else:
+            cart_item.quantity += serializer.validated_data['quantity']
+
+        cart_item.save()
+
+
+class CartItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return CartItem.objects.filter(
+            cart__customer=self.request.user
+        )
