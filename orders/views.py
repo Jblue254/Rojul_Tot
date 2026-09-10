81c1226744f968a2bdbd3d732b814192c49f93a6
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-10 20:47:57 +0300

    feat: implement OrderListCreateView and OrderDetailView with permissions and queryset logic

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Order
+from .serializers import OrderSerializer
+
+
+class OrderListCreateView(generics.ListCreateAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role in ['MANAGER', 'ADMIN']:
+            return Order.objects.all()
+
+        return Order.objects.filter(customer=user)
+
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user)
+
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role in ['MANAGER', 'ADMIN']:
+            return Order.objects.all()
+
+        return Order.objects.filter(customer=user)
