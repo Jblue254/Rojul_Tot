c2a93caf44f713aba8645fb5f94b88ced7ee0622
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-11 01:39:52 +0300

    feat: add create URL for NotificationCreateView

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NotificationListView, NotificationDetailView
+from .views import (NotificationListView,NotificationDetailView,NotificationCreateView,)
 
 
 urlpatterns = [
     path('', NotificationListView.as_view(), name='notification-list'),
+    path('create/', NotificationCreateView.as_view(), name='notification-create'),
     path('<int:pk>/', NotificationDetailView.as_view(), name='notification-detail'),
 ]
