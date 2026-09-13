1250736316ee76e9d8a92816647b933aa7b8a982
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-13 15:15:19 +0300

    feat: add UserStatisticsView URL to analytics urlpatterns

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DashboardStatisticsView
+from .views import DashboardStatisticsView, UserStatisticsView
 
 urlpatterns = [
     path('dashboard/',DashboardStatisticsView.as_view(),name='dashboard-statistics'),
+    path('users/',UserStatisticsView.as_view(),name='user-statistics'),
 ]
