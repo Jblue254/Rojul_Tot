f4aeb57ed60af4bac837feed3ce0c19a4d433fa8
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-13 15:25:21 +0300

    feat: add projects URL to analytics urlpatterns

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DashboardStatisticsView, UserStatisticsView, RentalOrderStatisticsView
+from .views import DashboardStatisticsView, UserStatisticsView, RentalOrderStatisticsView, ProjectStatisticsView
 
 urlpatterns = [
     path('dashboard/',DashboardStatisticsView.as_view(),name='dashboard-statistics'),
     path('users/',UserStatisticsView.as_view(),name='user-statistics'),
     path('rental-orders/',RentalOrderStatisticsView.as_view(),name='rental-order-statistics'),
-]
+    path('projects/',ProjectStatisticsView.as_view(),name='project-statistics'),
+]       
