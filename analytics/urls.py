c4e25304f6d912d986321e70dd51215b8f360944
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-13 15:31:45 +0300

    feat: add admin dashboard URL to analytics urlpatterns

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import DashboardStatisticsView, UserStatisticsView, RentalOrderStatisticsView, ProjectStatisticsView
+from .views import DashboardStatisticsView, UserStatisticsView, RentalOrderStatisticsView, ProjectStatisticsView,  AdminDashboardView
 
 urlpatterns = [
     path('dashboard/',DashboardStatisticsView.as_view(),name='dashboard-statistics'),
     path('users/',UserStatisticsView.as_view(),name='user-statistics'),
     path('rental-orders/',RentalOrderStatisticsView.as_view(),name='rental-order-statistics'),
     path('projects/',ProjectStatisticsView.as_view(),name='project-statistics'),
+    path('admin-dashboard/',AdminDashboardView.as_view(),name='admin-dashboard'),
 ]       
