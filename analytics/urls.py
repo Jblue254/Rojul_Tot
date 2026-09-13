1f2b22660defa6239e635414b82e3a388cb2be15
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-13 15:18:45 +0300

    feat: add rental order statistics URL to analytics urlpatterns

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DashboardStatisticsView, UserStatisticsView
+from .views import DashboardStatisticsView, UserStatisticsView, RentalOrderStatisticsView,
 
 urlpatterns = [
     path('dashboard/',DashboardStatisticsView.as_view(),name='dashboard-statistics'),
     path('users/',UserStatisticsView.as_view(),name='user-statistics'),
+    path('rental-orders/',RentalOrderStatisticsView.as_view(),name='rental-order-statistics'),
 ]
