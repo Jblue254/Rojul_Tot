095f66034dd619ad2ab8a852d6f876dd96f2bec0
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-13 21:05:31 +0300

    feat: enhance UserSerializer to support optional password field and implement create/update methods

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -34,6 +34,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=False,
+        min_length=8
+    )
+
     class Meta:
         model = User
         fields = [
@@ -41,16 +47,41 @@ class UserSerializer(serializers.ModelSerializer):
             'full_name',
             'email',
             'phone',
+            'password',
             'role',
             'profile_image',
             'address',
+            'is_active',
             'created_at',
             'updated_at',
         ]
         read_only_fields = [
             'id',
-            'email',
-            'role',
             'created_at',
             'updated_at',
         ]
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+
+        user = User(**validated_data)
+
+        if password:
+            user.set_password(password)
+
+        user.save()
+
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+
+        return instance
