7e1f8f3c4724fc14a0f98bf0fbbff9ef2ee82566
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-15 18:24:33 +0300

    feat: update URLs to include DrawingCategory views for listing and detail

--- drawings/urls.py
+++ drawings/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import DrawingListCreateView, DrawingDetailView
+
+from .views import DrawingCategoryListCreateView,DrawingCategoryDetailView,DrawingListCreateView,DrawingDetailView
+
 
 
 urlpatterns = [
-    path('', DrawingListCreateView.as_view(), name='drawing-list-create'),
-    path('<int:pk>/', DrawingDetailView.as_view(), name='drawing-detail'),
+    path('categories/',DrawingCategoryListCreateView.as_view(),name='drawing-category-list-create'),
+    path('categories/<int:pk>/',DrawingCategoryDetailView.as_view(),name='drawing-category-detail'),
+    path('',DrawingListCreateView.as_view(),name='drawing-list-create'),
+    path('<int:pk>/',DrawingDetailView.as_view(),name='drawing-detail'),
 ]
