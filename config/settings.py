6f9cc2987cfaa85eb5fd6277e4a06ff643228a60
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-16 20:00:44 +0300

    Refactor code structure for improved readability and maintainability

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o==6y0i39+ct4k(w4!9*fbbc8^e)-ju((j=_f^t9j%y@3tug23'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     ".onrender.com",
