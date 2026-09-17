3c40f3de8f6ed6a30c8611782ddec6e4e2132647
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-17 14:00:08 +0300

    feat: Load environment variables from .env file and update settings structure

--- config/settings.py
+++ config/settings.py
@@ -16,17 +16,17 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env
+load_dotenv(BASE_DIR / ".env")
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
