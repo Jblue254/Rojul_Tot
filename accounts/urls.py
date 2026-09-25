bdcd58219576b8693bfb5e390db9260e918fa687
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-25 10:53:58 +0300

    feat: Add ChangePasswordView to URL patterns and reorganize imports

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+
 from .views import (
     RegisterView,
     LoginView,
     ProfileView,
+    ChangePasswordView,
     AdminOnlyView,
     AdminUserListCreateView,
     AdminUserDetailView,
@@ -13,10 +14,19 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('profile/', ProfileView.as_view()),
+    path('change-password/', ChangePasswordView.as_view()),
+
     path('admin-only/', AdminOnlyView.as_view()),
 
-    path('users/', AdminUserListCreateView.as_view(), name='admin-user-list'),
-    path('users/<int:pk>/', AdminUserDetailView.as_view(), name='admin-user-detail'),
+    path(
+        'users/',
+        AdminUserListCreateView.as_view(),
+        name='admin-user-list'
+    ),
 
+    path(
+        'users/<int:pk>/',
+        AdminUserDetailView.as_view(),
+        name='admin-user-detail'
+    ),
 ]
-
